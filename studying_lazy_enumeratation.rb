dfd16dc468f019dbf495eaaed7218f55422551cb
marcelorxaviers <marcelorxs@gmail.com>
2016-03-07 09:41:38 -0300

    Correct tab size

--- studying_lazy_enumeratation.rb
+++ studying_lazy_enumeratation.rb
@@ -1,8 +1,8 @@
 fibonacci =
 Enumerator.new do |yielder, base: 1, number: 1|
   loop do
-  	yielder.yield number
-  	base, number = number, (base + number)
+    yielder.yield number
+   base, number = number, (base + number)
   end
 end
 fibonacci.select { |f| (f % 3).zero? }.first(10) # Infinite loop
@@ -11,14 +11,14 @@ fibonacci.select { |f| (f % 3).zero? }.first(10) # Infinite loop
 fibonacci =
 Enumerator.new do |yielder, base: 1, number: 1|
   loop do
-  	yielder.yield number
-  	base, number = number, (base + number)
+    yielder.yield number
+    base, number = number, (base + number)
   end
 end.lazy
 fibonacci.select { |f| (f % 3).zero? }.first(10)
 
 module Ras
-	extend self
+  extend self
   def ra
     puts "'s al Ghul"
   end
