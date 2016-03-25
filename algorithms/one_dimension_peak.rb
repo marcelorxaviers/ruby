74a9b780fe0bb283b93cddc35150acd8bded0c6e
marcelorxaviers <marcelograd@gmail.com>
2016-03-25 14:26:50 -0300

    Recursive mergesort

--- algorithms/one_dimension_peak.rb
+++ algorithms/one_dimension_peak.rb
@@ -6,16 +6,16 @@ end
 def on(a, from = 0, to = a.size - 1)
   a.first if a.size < 2
   from.upto(to) do |i|
-  	return a[i] if greater?(a[i], (a[i - 1] || a[i]), (a[i + 1] || a[i]))
+    return a[i] if greater?(a[i], (a[i - 1] || a[i]), (a[i + 1] || a[i]))
   end
 end
 
 def logn2(a)
-	a.first if a.size < 2
-	i = a.size/2
-	return logn2(a[0..(i - 1)]) if a[i] < (a[i - 1] || a[i])
-	return logn2(a[(i + 1)..-1]) if a[i] < (a[i + 1] || a[i])
-	a[i]
+  a.first if a.size < 2
+  i = a.size/2
+  return logn2(a[0..(i - 1)]) if a[i] < (a[i - 1] || a[i])
+  return logn2(a[(i + 1)..-1]) if a[i] < (a[i + 1] || a[i])
+  a[i]
 end
 
 a = 1.upto(10000000).to_a
